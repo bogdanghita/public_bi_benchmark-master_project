7d407bb5d5bd4515b6717d49a6ba2875735bec19
Bogdan Ghita <bogdan.ghita93@gmail.com>
2019-03-14 14:15:33 +0100

    save vectorwise load log for each table

--- scripts/vectorwise/2-load-data.sh
+++ scripts/vectorwise/2-load-data.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 WORKING_DIR=$(pwd)
+VW_LOG_FILE="/scratch/bogdan/vectorwise/ingres/files/vectorwise.log"
 
 
 usage() {
@@ -13,8 +14,8 @@ EOM
 }
 
 if [ "$#" -ne 2 ]; then
-	usage
-	exit 1
+        usage
+        exit 1
 fi
 DB_NAME="$1"
 DATASET_DIR="$2"
@@ -22,36 +23,50 @@ DATASET_DIR="$2"
 echo "help tm\g" | sql $DB_NAME > /dev/null
 ret=$?
 if [ "$ret" -ne 0 ]; then
-	echo "error: unable to connect to database"
-	exit $ret
+        echo "error: unable to connect to database"
+        exit $ret
 fi
 
 cd $DATASET_DIR
 ret=$?
 if [ $ret -ne 0 ]; then
-	exit $ret
+        exit $ret
 fi
 
 
 for wb in ./*
 do
-	for f in ./$wb/*.csv
-	do
-		echo "$(date) $f"
-		filename=$(basename -- "$f")
-		table="${filename%.*}"; table="${table%\"}"; table="${table#\"}"
-		# table="${filename%.sample*}"; table="${table%\"}"; table="${table#\"}"
+        for f in ./$wb/*.csv
+        do
+                echo "$(date) $f"
+                filename=$(basename -- "$f")
+                table="${filename%.*}"; table="${table%\"}"; table="${table#\"}"
+                # table="${filename%.sample*}"; table="${table%\"}"; table="${table#\"}"
 
-		# NOTE: set --errcount to the number of rows
-		# 		- this is neccessary because --errcount 0 does not work as expected (no rows are loaded if there are errors)
-		r="$(wc -l "$f")"; r="${r%% *}"
-		echo "$r rows"
+                # NOTE: set --errcount to the number of rows
+                #               - this is neccessary because --errcount 0 does not work as expected (no rows are loaded if there are errors)
+                r="$(wc -l "$f")"; r="${r%% *}"
+                echo "$r rows"
 
-        mkdir -p "$wb/load-vectorwise"
-		vwload --fdelim "|" --nullvalue "null" --errcount "$r" --log "$wb/load-vectorwise/$table.load.log" -z --table "$table" pbib "$f" > "$wb/load-vectorwise/$table.load.out" 2> "$wb/load-vectorwise/$table.load.err"
-		ret=$?
-		echo $ret > "$wb/load-vectorwise/$table.load.ret"
-	done
+                mkdir -p "$wb/stats-vectorwise"
+
+                #tail -n 1 $VW_LOG_FILE > "$wb/stats-vectorwise/$table.compression-log.before"
+                echo -n "" > $VW_LOG_FILE
+		sleep 2
+		echo "$(date) load start for table: $table" >> $VW_LOG_FILE
+
+                mkdir -p "$wb/load-vectorwise"
+                vwload --fdelim "|" --nullvalue "null" --errcount "$r" --log "$wb/load-vectorwise/$table.load.log" -z --table "$table" pbib "$f" > "$wb/load-vectorwise/$table.load.out" 2> "$wb/load-vectorwise/$table.load.err"
+
+                sleep 2
+		echo "$(date) load end for table: $table" >> $VW_LOG_FILE
+		sleep 2
+                #tail -n 1 $VW_LOG_FILE > "$wb/stats-vectorwise/$table.compression-log.after"
+                cat $VW_LOG_FILE > "$wb/stats-vectorwise/$table.compression-log.out"
+
+                ret=$?
+                echo $ret > "$wb/load-vectorwise/$table.load.ret"
+        done
 done
 
 
