43ffd752b68b7ded82ee353b8463b1c4dbb4a34d
Bogdan Ghita <bogdan.ghita93@gmail.com>
2019-04-08 14:24:03 +0200

    fixed indentation

--- scripts/vectorwise/2-load-data.sh
+++ scripts/vectorwise/2-load-data.sh
@@ -52,15 +52,15 @@ do
 
                 #tail -n 1 $VW_LOG_FILE > "$wb/stats-vectorwise/$table.compression-log.before"
                 echo -n "" > $VW_LOG_FILE
-		sleep 2
-		echo "$(date) load start for table: $table" >> $VW_LOG_FILE
+                sleep 2
+                echo "$(date) load start for table: $table" >> $VW_LOG_FILE
 
                 mkdir -p "$wb/load-vectorwise"
                 vwload --fdelim "|" --nullvalue "null" --errcount "$r" --log "$wb/load-vectorwise/$table.load.log" -z --table "$table" pbib "$f" > "$wb/load-vectorwise/$table.load.out" 2> "$wb/load-vectorwise/$table.load.err"
 
                 sleep 2
-		echo "$(date) load end for table: $table" >> $VW_LOG_FILE
-		sleep 2
+                echo "$(date) load end for table: $table" >> $VW_LOG_FILE
+                sleep 2
                 #tail -n 1 $VW_LOG_FILE > "$wb/stats-vectorwise/$table.compression-log.after"
                 cat $VW_LOG_FILE > "$wb/stats-vectorwise/$table.compression-log.out"
 
