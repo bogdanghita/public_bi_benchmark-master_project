b3b041e3a46589e9591db238777dbe4bac5ba099
Bogdan Ghita <bogdan.ghita93@gmail.com>
2019-04-11 16:33:54 +0200

    add tables vector-wise

---
+++ benchmark/Bimbo/tables-vectorwise/Bimbo_1.table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "Bimbo_1"(
+"Agencia_ID" smallint NOT NULL,
+"Canal_ID" smallint NOT NULL,
+"Cliente_ID" integer NOT NULL,
+"Demanda_uni_equil" smallint NOT NULL,
+"Dev_proxima" float8 NOT NULL,
+"Dev_uni_proxima" integer NOT NULL,
+"Number of Records" smallint NOT NULL,
+"Producto_ID" integer NOT NULL,
+"Ruta_SAK" smallint NOT NULL,
+"Semana" smallint NOT NULL,
+"Venta_hoy" float8 NOT NULL,
+"Venta_uni_hoy" smallint NOT NULL
+);
